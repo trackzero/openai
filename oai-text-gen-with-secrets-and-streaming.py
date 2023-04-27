a812ec2c37a942de522fa4839da8dea18562e186
Track Zero <trackzero@gmail.com>
2023-04-26 20:19:00 -0500

    Had ChatGPT add error handling and use env variable in the event of a Secrets Manager error.

--- oai-text-gen-with-secrets-and-streaming.py
+++ oai-text-gen-with-secrets-and-streaming.py
@@ -2,7 +2,6 @@ import os
 import json
 import openai
 from colorama import init, Fore, Style
-import boto3
 import botocore
 import botocore.session
 from botocore.exceptions import ClientError
@@ -10,28 +9,32 @@ from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 
 model="gpt-3.5-turbo"     #"gpt-4" if you have it.
 
-#retreive API key from AWS Secrets Manager
+# Retrieve API key from AWS Secrets Manager, or try environment variable
 def get_secret():
-
     secret_name = "openai_api_key"
     region_name = "us-west-2"
 
-    #create boto client/session
-    client = botocore.session.get_session().create_client('secretsmanager', region_name=region_name)
-    cache_config = SecretCacheConfig()
-    cache = SecretCache( config = cache_config, client = client)
+    # Create boto client/session
+    try:
+        client = botocore.session.get_session().create_client('secretsmanager', region_name=region_name)
+        cache_config = SecretCacheConfig()
+        cache = SecretCache( config = cache_config, client = client)
 
-    #retrieve secret and cache
-    secret = cache.get_secret_string(secret_name)
-    secret_dict = json.loads(secret)
-    
-    return secret_dict['openai_api_key']
+        # Retrieve secret and cache
+        secret = cache.get_secret_string(secret_name)
+        secret_dict = json.loads(secret)
 
-# what's the scientific name for a platypus?
-
-#openai.organization = "org-placeholder"
-#Get API key from AWS Secrets Manager
-openai.api_key = get_secret()
+        return secret_dict['openai_api_key']
+    except ClientError as e:
+        # Print error if secret retrieval fails
+        print(Fore.YELLOW + f"Error retrieving secret: {e}" + Style.RESET_ALL)
+        # Try environment variable instead
+        openai_api_key = os.environ.get('OPENAI_API_KEY')
+        if openai_api_key:
+            print(Fore.YELLOW + "Trying environment variable instead of AWS Secrets Manager" + Style.RESET_ALL)
+            return openai_api_key
+        else:
+            print(Fore.RED + "Environment variable OPENAI_API_KEY not found" + Style.RESET_ALL)
 
 # Set up initial conversation context
 conversation = []
@@ -42,30 +45,31 @@ init()
 # Create an instance of the ChatCompletion API
 def chatbot(conversation):
     max_tokens=1024
-    response= openai.ChatCompletion.create(
-        model=model,
-        messages=conversation,
-        max_tokens=max_tokens,
-        stream=True)
-    
-    #event variables
-    collected_chunks=[]
-    collected_messages = ""
+    try:
+        response= openai.ChatCompletion.create(
+            model=model,
+            messages=conversation,
+            max_tokens=max_tokens,
+            stream=True)
+        
+        #event variables
+        collected_chunks=[]
+        collected_messages = ""
 
-    #capture and print event stream
-    print(Fore.YELLOW + "Bot: " + Style.RESET_ALL)
-    for chunk in response:
-        collected_chunks.append(chunk)  # save the event response
-        chunk_message = chunk['choices'][0]['delta']  # extract the message
-        if "content" in chunk_message:
-            message_text = chunk_message['content']
-            collected_messages += message_text
-            print(f"{message_text}", end="")
-    print(f"\n")
-    return(collected_messages)
-
-
-   
+        #capture and print event stream
+        print(Fore.YELLOW + "Bot: " + Style.RESET_ALL)
+        for chunk in response:
+            collected_chunks.append(chunk)  # save the event response
+            chunk_message = chunk['choices'][0]['delta']  # extract the message
+            if "content" in chunk_message:
+                message_text = chunk_message['content']
+                collected_messages += message_text
+                print(f"{message_text}", end="")
+        print(f"\n")
+        return(collected_messages)
+    except Exception as e:
+        # Print error if chatbot fails to generate response
+        print(Fore.RED + f"Error generating chat response: {e}" + Style.RESET_ALL)
 
 # Print welcome message and instructions
 print(Fore.GREEN + "Welcome to the chatbot! To start, enter your message below.")
@@ -74,20 +78,27 @@ print("To stop, say 'stop','exit', or 'bye'" + Style.RESET_ALL)
 
 # Loop to continuously prompt user for input and get response from OpenAI
 while True:
-    user_input = input(Fore.CYAN + "User: " + Style.RESET_ALL)
-    if user_input.lower() in ["reset", "let's start over"]:
-        conversation = []
-        print(Fore.YELLOW + "Bot: Okay, let's start over." + Style.RESET_ALL)
-        
-    elif user_input.lower() in ["stop", "exit", "bye", "quit", "goodbye"]:
+    try:
+        user_input = input(Fore.CYAN + "User: " + Style.RESET_ALL)
+        if user_input.lower() in ["reset", "let's start over"]:
+            conversation = []
+            print(Fore.YELLOW + "Bot: Okay, let's start over." + Style.RESET_ALL)
+
+        elif user_input.lower() in ["stop", "exit", "bye", "quit", "goodbye"]:
+            print(Fore.RED + Style.BRIGHT + "Bot: Okay, goodbye!" + Style.RESET_ALL)
+            break
+        else:
+            # Append user message to conversation context
+            conversation.append({"role": "user", "content": user_input})
+            # Generate chat completion
+            chat = chatbot(conversation)
+
+            # Append bot message to conversation context
+            conversation.append({"role": "assistant", "content": chat})
+    except KeyboardInterrupt:
+        # Handle keyboard interrupt gracefully
         print(Fore.RED + Style.BRIGHT + "Bot: Okay, goodbye!" + Style.RESET_ALL)
         break
-    else:
-        # Append user message to conversation context
-        conversation.append({"role": "user", "content": user_input})
-        # Generate chat completion
-        chat = chatbot(conversation)
-        
-        # Append bot message to conversation context
-        conversation.append({"role": "assistant", "content": chat})
-
+    except Exception as e:
+        # Print error if an unexpected exception occurs
+        print(Fore.RED + f"Unexpected error: {e}" + Style.RESET_ALL)
