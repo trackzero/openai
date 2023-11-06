9a3f635edcf253e7e729072b5da15c637b0f3af8
TrackZero <trackzero@gmail.com>
2023-11-06 17:41:09 -0600

    Updated to dall-e-3

--- oai-generate-image.py
+++ oai-generate-image.py
@@ -11,12 +11,13 @@ from io import BytesIO
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_image(prompt):
-    model = "image-alpha-001"
+    model = "dall-e-3" #was: "image-alpha-001"
     response_format = "url"
     num_images = 1
-    size = "512x512"
+    size = "1024x1024" 
+    quality="standard"
     image = openai.Image.create(model=model, prompt=prompt, response_format=response_format,
-                                 num_images=num_images, size=size)
+                                 num_images=num_images, size=size, quality=quality)
     return image['data'][0]['url']
 
 prompt = input("Enter a prompt: ")
